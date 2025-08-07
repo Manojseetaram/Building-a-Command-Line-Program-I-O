4020f96fe12ee78a0f80a55221008187fdd0ca03
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-08-07 15:38:50 +0530

    create project datatypes models

--- src/models.rs
+++ src/models.rs
@@ -1,14 +1,33 @@
 use chrono::NaiveDate;
+use serde::{Deserialize, Serialize};
 
+
+#[derive(Debug , Serialize , Deserialize , Clone)]
 pub enum Priority {
     Low,
     Medium,
     High
 }
+#[derive(Debug , Serialize , Deserialize , Clone)]
 pub struct Task{
     pub title : String,
     pub description : String,
     pub due_date : NaiveDate,
     pub priority : Priority,
     pub completed : bool
+}
+impl Task(){
+    pub fn new(title : String , description : String , due_date : NaiveDate , priority : Priority )-> Self{
+    Task{
+       title ,
+       description,
+       due_date,
+       priority,
+       completed:false
+    }
+
+    }
+    pub fn mark_completed(&mut self){
+        self.completed = true;
+    }
 }
