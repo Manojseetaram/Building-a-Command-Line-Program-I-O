c028e4486a033c48fdbd62237fa7e3c2e790407d
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-08-07 18:27:37 +0530

    added a task

--- src/tests.rs
+++ src/tests.rs
@@ -1,8 +1,35 @@
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use chrono::NaiveDate;
+    use crate::models::{Priority, Task};
 
     
+    use chrono::NaiveDate;
+
+
+    #[test]
+    fn test_task_creation(){
+        let task = Task::new(
+        "Test".to_string(),
+        "Test task".to_string(),
+        NaiveDate::from_ymd_opt(2025, 05, 19).unwrap(),
+        Priority::Medium
+        );
+        assert_eq!(task.title , "Test");
+        assert!(!task.completed)
+    }
+    #[test]
+    fn test_task_completed(){
+        let mut task = Task::new(
+            "Test".to_string(),
+            "Test task".to_string(),
+            NaiveDate::from_ymd_opt(2025, 05, 19).unwrap(),
+            Priority::Low
+        );
+        task.mark_complete();
+        assert!(task.completed)
+    }
+
+
+
 
 }
